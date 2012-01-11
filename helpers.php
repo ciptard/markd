1446eec4cbfc4032c83c182705d189faabf572aa
Matt Walters <mwalters8@gmail.com>
2012-01-11 15:33:35 -0500

    Refactors Helpers to be a Class with static methods
    Refactors Helpers to be a Class with static methods for code
    organizational purposes.

--- helpers.php
+++ helpers.php
@@ -1,51 +1,13 @@
 <?php
 
-// TODO - Refactor into a class with static methods [mwalters :: 2012-01-10] 
-
-function list_directory($dir, $count = null, $offset = 0) {
-	$file_list = '';
-	$stack[] = $dir;
-	$ctr = 0;
-	while ($stack) {
-		$current_dir = array_pop($stack);
-		if ($dh = opendir($current_dir)) {
-			while (($file = readdir($dh)) !== false) {
-				if ($file !== '.' AND $file !== '..' AND $file != '.DS_Store') {
-					$current_file = "{$current_dir}/{$file}";
-					if (is_file($current_file)) {
-						$ctr++;
-						if ($ctr > $offset) {
-							$file_list[] = "{$current_dir}/{$file}";
-						}
-						if ($ctr == ($offset + $count)) {
-							return $file_list;
-						}
-					} elseif (is_dir($current_file)) {
-						$stack[] = $current_file;
-					}
-				}
-			}
-		}
-	}
-	return $file_list;
-}
-
-function sort_multidimensional($sortField, $toSorts, $order = 'ASC') {
-	if ($order == 'DESC') { $order = SORT_DESC; } else { $order = SORT_ASC; }
-
-	$keyIndex = array();
-	foreach ($toSorts as $toSort) {
-		if (is_object($toSort)) {
-			$keyIndex[] = $toSort->$sortField;
-		} else {
-			$keyIndex[] = $toSort[$sortField];
-		}
-	}
-	array_multisort($keyIndex, $order, $toSorts);
-
-	return $toSorts;
-}
-
+/**
+ * _debug() Function
+ * 
+ * This is specifically left out of the Helpers class because of developer preference for wanting to be able to just quickly type it.
+ * 
+ * @param mixed Any string, object, or array to be displayed to be printed out
+ * @return null
+ **/
 function _debug($data) {
 	echo '<div style="border: 1px solid #f00; padding: 5px; margin: 2px;">';
 	if (is_array($data) OR is_object($data)) {
@@ -56,4 +18,54 @@ function _debug($data) {
 		echo $data;
 	}
 	echo '</div>';
+	return null;
+}
+
+/**
+* Helpers Class
+*/
+class Helpers {
+	public static function list_directory($dir, $count = null, $offset = 0) {
+		$file_list = '';
+		$stack[] = $dir;
+		$ctr = 0;
+		while ($stack) {
+			$current_dir = array_pop($stack);
+			if ($dh = opendir($current_dir)) {
+				while (($file = readdir($dh)) !== false) {
+					if ($file !== '.' AND $file !== '..' AND $file != '.DS_Store') {
+						$current_file = "{$current_dir}/{$file}";
+						if (is_file($current_file)) {
+							$ctr++;
+							if ($ctr > $offset) {
+								$file_list[] = "{$current_dir}/{$file}";
+							}
+							if ($ctr == ($offset + $count)) {
+								return $file_list;
+							}
+						} elseif (is_dir($current_file)) {
+							$stack[] = $current_file;
+						}
+					}
+				}
+			}
+		}
+		return $file_list;
+	}
+	
+	public static function sort_multidimensional($sortField, $toSorts, $order = 'ASC') {
+		if ($order == 'DESC') { $order = SORT_DESC; } else { $order = SORT_ASC; }
+
+		$keyIndex = array();
+		foreach ($toSorts as $toSort) {
+			if (is_object($toSort)) {
+				$keyIndex[] = $toSort->$sortField;
+			} else {
+				$keyIndex[] = $toSort[$sortField];
+			}
+		}
+		array_multisort($keyIndex, $order, $toSorts);
+
+		return $toSorts;
+	}
 }
