8e79cc24815bb6d1e0658e014323347db1e15bbb
Matt Walters <mwalters8@gmail.com>
2012-01-11 19:14:48 -0500

    Adds constant for debug feature
    When set to true, debug information will be displayed.

--- config-sample.php
+++ config-sample.php
@@ -4,3 +4,4 @@ define('POSTS_PATH', '/path/to/your/markdown/posts');
 define('PUBLISHED_PATH', '/path/to/where/you/want/the/site/published');
 define('POSTS_PER_PAGE', 10);
 define('POSTS_SORT_ORDER', 'DESC');
+define('MARKD_DEBUG', FALSE);
