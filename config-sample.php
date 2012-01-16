678956652f6a2ef7e7e0c5a92afd9518484be7ee
Matt Walters <mwalters8@gmail.com>
2012-01-15 20:30:18 -0500

    Adding definitions for path to themes and plugins folders

--- config-sample.php
+++ config-sample.php
@@ -2,6 +2,8 @@
 date_default_timezone_set('America/New_York');
 define('POSTS_PATH', '/path/to/your/markdown/posts');
 define('PUBLISHED_PATH', '/path/to/where/you/want/the/site/published');
+define('THEMES_PATH', '/path/to/themes');
+define('PLUGINS_PATH', '/path/to/plugins');
 define('POSTS_PER_PAGE', 10);
 define('POSTS_SORT_ORDER', 'DESC');
 define('MARKD_DEBUG', FALSE);
