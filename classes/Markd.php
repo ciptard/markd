9496e8c5d6226844faf2f744af9328c1b08de6ba
Matt Walters <mwalters8@gmail.com>
2012-01-16 18:38:56 -0500

    Large refactor of Markd and Posts Classes. Closes #10
    The Posts Class was doing too much for what it was.  The "driving" of
    things was refactored into the Markd Class.  This also allowed for
    making sure that both unpublished posts did not make their way into
    post listings, and that pages of posts would always have the correct
    number of POSTS_PER_PAGE on them.

--- classes/Markd.php
+++ classes/Markd.php
@@ -2,11 +2,74 @@
 /**
 * markd Class
 * 
-* This is really just a wrapper and put in to allow a back-door for adding functionality if needed in the future.
-* 
 */
 class Markd {
+	public $publishedPosts;
+	public $filesWritten;
+	private $currentPage;
+	
 	function __construct() {
-		$t = new Posts();
+		$this->publishedPosts = 0;
+		$this->filesWritten = 0;
+		$this->currentPage = 0;
+
+		// Process blog posts
+		$currently_processing = TRUE;
+
+		$lastPublished = 0;																	// See Posts Class for a reasonable explanation of $lastPublished
+		while ($currently_processing) {
+			$blogPosts = array();
+			$get_posts = $this->get_posts($lastPublished, POSTS_PER_PAGE);
+			$lastPublished = $get_posts['lastPublished'];
+			$blogPosts = $get_posts['blogPosts'];
+
+			$this->write_file($this->currentPage, $blogPosts);
+			if (count($blogPosts) < POSTS_PER_PAGE) { $currently_processing = FALSE; }		// Keep loop going until we reach a full page of posts
+			$this->currentPage++;
+		}
+		
+		$this->complete_process();
+	}
+	
+	public function get_posts($startPostNum, $numberOfPosts) {
+		$posts = Posts::get_posts($startPostNum, $numberOfPosts);
+		$this->publishedPosts = $this->publishedPosts + count($posts['blogPosts']);
+
+		return $posts;
+	}
+	
+	public function get_pages() { } // Stub
+	
+	public function write_file($pageNumber, $contentList) {
+		if (count($contentList) < 1) { return FALSE; }
+		
+		if ($pageNumber == 0) {
+			$file = PUBLISHED_PATH . '/index.html';
+		} else {
+			$file = PUBLISHED_PATH . '/archive-' . $pageNumber . '.html';
+		}
+		
+		$fp = fopen($file, 'w');
+
+		$header = Helpers::locate_template('header');
+		fwrite($fp, $header);
+		
+		if (!empty($contentList)) {
+			foreach($contentList as $content) {
+				fwrite($fp, $content->html_content);
+			}
+		}
+
+		$footer = Helpers::locate_template('footer');
+		fwrite($fp, $footer);
+		fclose($fp);
+		
+		$this->filesWritten++;
+	}
+	
+	private function complete_process() {
+		echo "\n\nProcessed " . $this->publishedPosts . " posts.";
+		echo "\nWrote " . $this->filesWritten . " files.";
+		echo "\n\n";
 	}
 }
--- classes/Posts.php
+++ classes/Posts.php
@@ -3,98 +3,37 @@
 * Posts Class
 */
 class Posts {
-	private $post_listing;				// Array of Post files
-	private $posts;						// Post objects
-	private $published_posts;			// Count of published posts
-	private $unpublished_posts;			// Count of unpublished posts
-	private $current_page;				// Pagination tracking
-	
-	function __construct() {
-		$this->published_posts = 0;
-		$this->unpublished_posts = 0;
-		$this->current_page = 0;
-		$this->process_posts();
-	}
-	
-	private function process_posts() {
-		$currently_processing = true;
-		while ($currently_processing) {			// Loop through posts to write out front page and archive pages
-			$this->post_listing = array();
-			$this->post_listing = Helpers::list_directory(POSTS_PATH, POSTS_PER_PAGE, ($this->current_page * POSTS_PER_PAGE));
-			$this->parse_posts();
-			if ($this->current_page == 0) {
-				$this->write_front_page();
-			} else {
-				$this->write_archive_page();
-			}
-			if (count($this->post_listing) < POSTS_PER_PAGE) {
-				$currently_processing = false;
-			} else {
-				$this->current_page++;
-			}
-		}
-		$this->complete_process();
-	}
-	
-	public function parse_posts() {				// Create Post objects
-		$this->posts = array();
-		if (!empty($this->post_listing)) {
-			foreach ($this->post_listing as $k=>$post_file) {
-				unset($post);
-				$post = new Post($post_file);
-				if ($post->published == 'false') {
-					$this->unpublished_posts++;
-				} else {
-					$this->published_posts++;
-				}
-				if ($post->published != 'false') {
-					$this->posts[] = $post;
+	public static function get_posts($startPostNum, $numberOfPosts) {
+		if ($startPostNum === '' || $numberOfPosts == '' || $numberOfPosts < 1) { return FALSE; }
+
+		$returnPostListing = array();
+
+		/*
+		Last published is a bit interesting.  Essentially the deal is that unpublished posts can exist in the posts directory.  In order to
+		conserve on memory usage, we only grab and process POSTS_PER_PAGE number of posts at a time.  This makes it possible that a page of
+		posts could end up with less than POSTS_PER_PAGE on a page, so $lastPublished is used to track an index position of where we are in
+		the total file list so that when this loop is entered into again, we can start off at the proper place.
+		*/
+
+		$lastPublished = $startPostNum;
+
+		do {
+			$postListing = Helpers::list_directory(POSTS_PATH, $numberOfPosts, $lastPublished);
+
+			if (!empty($postListing)) {
+				foreach ($postListing as $postFile) {
+					$post = new Post($postFile);
+					if ($post->published == 'true' && count($returnPostListing['blogPosts']) < $numberOfPosts) {
+						$returnPostListing['blogPosts'][] = $post;
+					}
+					if (count($returnPostListing['blogPosts']) < $numberOfPosts) { $lastPublished++; }
 				}
 			}
-		}
-		$this->posts = Helpers::sort_multidimensional('date', $this->posts, POSTS_SORT_ORDER);
-	}
-	
-	public function write_front_page() {		// Write out Front Page
-		$fp = fopen(PUBLISHED_PATH . '/index.html', 'w');
-
-		$header = Helpers::locate_template('header');
-		fwrite($fp, $header);
+			
+			$startPostNum = $startPostNum + $numberOfPosts;
+		} while (count($returnPostListing['blogPosts']) < $numberOfPosts && count($postListing) == $numberOfPosts);
 		
-		if (!empty($this->posts)) {
-			foreach($this->posts as $post) {
-				$fp = fopen(PUBLISHED_PATH . '/index.html', 'a');
-				fwrite($fp, $post->html_content);
-				fclose($fp);
-			}
-		}
-
-		$fp = fopen(PUBLISHED_PATH . '/index.html', 'a');		
-		$footer = Helpers::locate_template('footer');
-		fwrite($fp, $footer);
-	}
-	
-	public function write_archive_page() {		// Write out archive page
-		$fp = fopen(PUBLISHED_PATH . '/archive-' . $this->current_page . '.html', 'w');
-
-		$header = Helpers::locate_template('header');
-		fwrite($fp, $header);
-
-		if (!empty($this->posts)) {
-			foreach($this->posts as $post) {
-				$fp = fopen(PUBLISHED_PATH . '/archive-' . $this->current_page . '.html', 'a');
-				fwrite($fp, $post->html_content);
-				fclose($fp);
-			}
-		}
-		
-		$fp = fopen(PUBLISHED_PATH . '/archive-' . $this->current_page . '.html', 'a');	
-		$footer = Helpers::locate_template('footer');
-		fwrite($fp, $footer);
-	}
-	
-	private function complete_process() {		// Display statistics
-		echo "\n\nProcessed " . ($this->published_posts + $this->unpublished_posts) . " posts (Published: $this->published_posts / Unpublished: $this->unpublished_posts).";
-		echo "\n\n";
+		$returnPostListing['lastPublished'] = $lastPublished + 1;
+		return $returnPostListing;
 	}
 }
