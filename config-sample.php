922aad2dc54c815507398cb770ec8a6058fabfaa
Matt Walters <mwalters8@gmail.com>
2012-01-17 20:52:53 -0500

    Just commenting the config file

--- config-sample.php
+++ config-sample.php
@@ -1,15 +1,18 @@
 <?php
-date_default_timezone_set('America/New_York');
-define('POSTS_PATH', '/path/to/your/markdown/posts');
-define('PUBLISHED_PATH', '/path/to/where/you/want/the/site/published');
-define('THEMES_PATH', '/path/to/themes');
-define('PLUGINS_PATH', '/path/to/plugins');
-define('POSTS_PER_PAGE', 10);
-define('POSTS_SORT_ORDER', 'DESC');
-define('MARKD_DEBUG', FALSE);
 
-define('SITE_TITLE', '');
-define('SITE_URL', '');
-define('SITE_DESC', '');
+// Paths and URL's should not have trailing slashes
 
-define('ACTIVE_THEME', '/default');
+date_default_timezone_set('America/New_York');										// Timezone
+define('POSTS_PATH', '/path/to/your/markdown/posts');                               // Path to where your MarkDown posts are at
+define('PUBLISHED_PATH', '/path/to/where/you/want/the/site/published');             // Path to where generated files should be placed
+define('THEMES_PATH', '/path/to/themes');                                           // Path to themes
+define('PLUGINS_PATH', '/path/to/plugins');                                         // Path to plugins
+define('POSTS_PER_PAGE', 10);                                                       // Number of posts per page for post listing pages
+define('POSTS_SORT_ORDER', 'DESC');                                                 // Sort posts in ASCending or DESCinding order?
+define('MARKD_DEBUG', FALSE);                                                       // Can be used to turn on debug notices during generation
+                                                                                    
+define('SITE_TITLE', '');                                                           // Title for site
+define('SITE_URL', '');                                                             // URL the generated site will sit at
+define('SITE_DESC', '');                                                            // (Optional) Description/tagline for site
+                                                                                    
+define('ACTIVE_THEME', '/default');                                                 // Folder name of the active theme
